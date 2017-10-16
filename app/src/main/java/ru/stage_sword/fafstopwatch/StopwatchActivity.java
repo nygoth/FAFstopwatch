dc8e6a1c69b01daf2b6e3e974a99bfd13389d6bb
Sergei Dryannyikh <nygoth@gmail.com>
2017-10-17 00:15:53 +0700

    Реализовал экран настроек и два собственных контрола настроек -- NumberPicker и TimePicker Настройки пока никак в основном коде не читаются и не используются.

--- app/src/main/java/ru/stage_sword/fafstopwatch/StopwatchActivity.java
+++ app/src/main/java/ru/stage_sword/fafstopwatch/StopwatchActivity.java
@@ -2,6 +2,7 @@ package ru.stage_sword.fafstopwatch;
 
 import android.annotation.SuppressLint;
 import android.content.Context;
+import android.content.Intent;
 import android.graphics.drawable.TransitionDrawable;
 import android.os.Bundle;
 import android.os.Handler;
@@ -23,6 +24,8 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.RadioGroup;
 import android.widget.TextView;
+
+import ru.stage_sword.preferences.StopwatchPreferences;
 
 import static ru.stage_sword.fafstopwatch.PrecisionChronometer.DELAYED;
 import static ru.stage_sword.fafstopwatch.PrecisionChronometer.ON_OFF;
@@ -472,5 +475,6 @@ public class StopwatchActivity extends AppCompatActivity
     }
 
     public void ShowSettings(MenuItem item) {
+        startActivity(new Intent(this, StopwatchPreferences.class));
     }
 }
